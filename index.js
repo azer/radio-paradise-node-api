445ccb1b11416ebb485f150798369765a832eb1e
E.Azer Koçulu <azer@kodfabrik.com>
2014-01-29 05:19:37 -0800

    use memoize-with-leveldb

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var debug = require("debug")('radio-paradise');
 var circle = require("circle");
 var scrape = require("scrape-url");
-var cache = require("level-json-cache")('nowplaying');
 var strip = require("strip");
+var memoize = require('memoize-with-leveldb')('data-songs');
+var nowplaying = memoize(pull, '90 seconds');
 
 module.exports = circle({
   '/': home
@@ -12,36 +13,23 @@ function home (reply) {
   nowplaying(reply);
 }
 
-function nowplaying (callback) {
-  cache.get('nowplaying', function (error, songs) {
-    if (!error) return callback(undefined, songs);
-
-    pull(function (error, songs) {
-      if (error) {
-        debug('Failed to fetch: %s', error.message);
-        return callback(error);
-      }
-
-      cache.set('nowplaying', songs, '90 seconds', function (error) {
-        if (error) {
-          debug('Failed to cache: %s', error.message);
-          return callback(error);
-        }
-
-        callback(undefined, songs);
-      });
-    });
-  });
-}
-
 function pull (callback) {
   debug('Pulling songs...');
 
-  scrape('http://www.radioparadise.com/ajax_rp2_playlist.php', '#pre_list .song_title', function (error, links) {
+  scrape('http://www.radioparadise.com/ajax_rp2_playlist.php', '.song_title', function (error, links) {
     if (error) return callback(error);
 
-    callback(undefined, links.map(function (el) {
-      return strip(el.innerHTML);
-    }));
+    links = links
+      .filter(isJustText)
+      .map(function (el) {
+        return el.innerHTML.replace(/<[^>]+>/g, '');
+      })
+      .reverse();
+
+    callback(undefined, links);
   });
+}
+
+function isJustText (el) {
+  return !/\<img/.test(el.innerHTML);
 }
